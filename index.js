027485237fa83c949274da33c03238c018630054
Tommy Ho <tommyho@cpp.edu>
2021-12-09 19:01:36 -0800

    run prettier

--- index.js
+++ index.js
@@ -3,15 +3,14 @@
 // import "https://unpkg.com/open-props/open-props.tokens.json";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const btnMore = document.querySelector("#btn-more");
-    const aboutMe = document.querySelector("#about-me");
+  const btnMore = document.querySelector("#btn-more");
+  const aboutMe = document.querySelector("#about-me");
 
-    btnMore.onclick = () => {
-        if (aboutMe.style.display !== "flex") {
-            aboutMe.style.display = "flex";
-        } else {
-            aboutMe.style.display = "none";
-        }
-
+  btnMore.onclick = () => {
+    if (aboutMe.style.display !== "flex") {
+      aboutMe.style.display = "flex";
+    } else {
+      aboutMe.style.display = "none";
     }
+  };
 });
