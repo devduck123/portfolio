ae024815cfd16fb0020bf7afc99834fd1c77a5b3
Tommy Ho <tommyho@cpp.edu>
2021-12-09 18:59:27 -0800

    setup about-me section

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-import "https://unpkg.com/open-props";
-import "https://unpkg.com/open-props/normalize.min.css";
-import "https://unpkg.com/open-props/open-props.tokens.json";
+// import "https://unpkg.com/open-props";
+// import "https://unpkg.com/open-props/normalize.min.css";
+// import "https://unpkg.com/open-props/open-props.tokens.json";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const btnMore = document.querySelector("#btn-more");
+    const aboutMe = document.querySelector("#about-me");
+
+    btnMore.onclick = () => {
+        if (aboutMe.style.display !== "flex") {
+            aboutMe.style.display = "flex";
+        } else {
+            aboutMe.style.display = "none";
+        }
+
+    }
+});
